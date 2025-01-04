b402c02d53afa4b34524d05802c4a5cb4f25c2f6
jan <jszczepanski75@gmail.com>
2025-01-04 21:16:55 +0100

    quick fix

--- backend/routes/lectureRoutes.js
+++ backend/routes/lectureRoutes.js
@@ -1,9 +1,7 @@
 import express from 'express';
-import multer from 'multer';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { createLecture, getLectures, deleteLecture, updateLecture } from '../controllers/lectureController.js';
 
-const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
 router.get("/:course_id", getLectures);
